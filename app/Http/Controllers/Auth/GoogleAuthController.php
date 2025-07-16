fb395c6dfd8b099dad4500b16d332b47d875b92c
MohammadSaleh Sedighi <m.saleh.sedeghi@gmail.com>
2025-07-16 09:51:49 +0330

    login with Google debug

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+use SweetAlert2\Laravel\Swal;
 
 class GoogleAuthController extends Controller
 {
@@ -19,21 +21,35 @@ class GoogleAuthController extends Controller
         try{
             $googleUser = Socialite::driver('google')->user();
 
-            $user = User::updateOrCreate([
-                'id' => $googleUser->getId(),
-            ],[
-                'name' => $googleUser->getName(),
-                'email' => $googleUser->getEmail(),
-                'password' => bcrypt(\Str::random(16)), // Generate a random password
-                'google_token' => $googleUser->token,
-                'google_refresh_token' => $googleUser->refreshToken,
-            ]);
+            // Check if the user already exists in the database
+            if (User::where('email', $googleUser->getEmail())->exists()) {
+                $user = User::where('email', $googleUser->getEmail())->first();
+            } else {
+                $user = User::updateOrCreate([
+                    'id' => $googleUser->getId(),
+                ],[
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => bcrypt(\Str::random(16)), // Generate a random password
+//                    'google_token' => $googleUser->token,
+//                    'google_refresh_token' => $googleUser->refreshToken,
+                ]);
 
+            }
             Auth::login($user);
 
+
             return redirect('/');
+
         }catch (\Exception $exception){
-            return $exception->getMessage();
+            $mesage = $exception->getMessage();
+            Swal::fire([
+                'title' => 'Error',
+                'text' => "Login with google was not seccessfully - $mesage",
+                'icon' => 'error',
+                'confirmButtonText' => 'OK'
+            ]);
+            return redirect('/login');
         }
 
     }
